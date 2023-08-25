bf347f3a0fe073e8a1a0526eb51e20bc1836e201
avilagabriella <avilagabriella84@gmail.com>
2023-08-24 22:49:11 -0300

    lista3

--- Fpoo/Atividade1.c
+++ Fpoo/Atividade1.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
 
-int main() {
+int main () {
 	
-    float preco;
-	float preco_final;
-
-    printf("Digite o pre?o do produto: ");
-    scanf("%f", &preco);
-
-    if (preco > 1000) {
-    	
-        preco_final = preco * 0.92; 
-        
-    } else {
-    	
-        preco_final = preco;
-        
-    }
-    
-    printf("Pre?o final: %.2f\n", preco_final);
-
-    return 0;
+	int i;
+	
+	for (i = 10; i <= 200; i++) {
+		
+		printf("%d \n", i);
+		
+	}
+	
+	return 0;
 }
-
